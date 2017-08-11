d22038443be6300517119acd1a2d5ff2f90f8f64
Cristian <cristiangjuan@gmail.com>
2017-08-11 12:37:22 +0200

    Initial commit

---
+++ app/src/main/java/com/android/cuentacuentosinteractivo/customs/CustomPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.android.cuentacuentosinteractivo.customs;
+
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
+
+import com.android.cuentacuentosinteractivo.activities.ReadFragment;
+import com.android.cuentacuentosinteractivo.utils.Constants;
+
+
+/**
+ * Adaptador para el ViewPager
+ */
+public class CustomPagerAdapter extends FragmentStatePagerAdapter {
+	private final int mSize;
+	
+	public CustomPagerAdapter(FragmentManager fm, int size) {
+        super(fm);
+        
+        Log.v(Constants.Log.METHOD, "CustomPagerAdapter - new()");
+        
+        mSize = size;
+    }
+
+    @Override
+    public int getCount() {
+    	//Log.v(Constants.Log.METHOD, "getCount CustomPagerAdapter");
+    	
+        return mSize;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+    	Log.v(Constants.Log.METHOD, "CustomPagerAdapter - getItem()");
+    	
+        return ReadFragment.newInstance(position);
+    }
+    
+}
